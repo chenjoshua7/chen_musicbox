6333acff12d7957c788193ef343f5df7e5947bc4
Joshua Chen <chen.joshua98@gmail.com>
2024-08-13 12:35:26 +0200

    cleaning raw data

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,24 +1,37 @@
 import pandas as pd
 import numpy as np
+import time
 
 def load_data(path: str = "song_lyrics.csv") -> pd.DataFrame:
     print("Loading Data...")
+    start = time.time()
     data = pd.read_csv(path)
-    print("Data Loaded")
+    end = time.time()
+    print(f"Data Loaded: {start-end}")
     return data
 
 def filter_data(data: pd.DataFrame) -> pd.DataFrame:
     df_ = data.copy()
     
     # Removing data less than 1000 views
+    start = time.time()
     df_ = df_[df_.views > 1000]
+    end = time.time()
+    print(f"Views Filtered: {start - end}")
+    
     # Removing misc. Some strange samples including the poetry of William Blake
+    start = time.time()
     df_ = df_[(df_.tag != "misc")]
+    end = time.time()
+    print(f"Misc Filtered: {start - end}")
     
     # Only including decades between 1950 and 2030. This removes erroneous data as well as
     # data unimport to the project
+    start = time.time()
     decade = df_.year//10 * 10
     df_[(decade >= 1950) & (decade <= 2030)]
+    end = time.time()
+    print(f"Decades Filtered: {start - end}")
     
     print(f"Original Samples: {data.shape[0]} \n Filtered Samples: {df_.shape[0]}")
     return df_
